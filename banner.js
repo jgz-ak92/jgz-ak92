7886856bf3068cc064bbbba032f8e84730e741ec
jgz-ak92 <stefan.kauffmann@hotmail.de>
2026-04-30 08:43:51 +0200

    Add banner loading functionality on page load

---
+++ banner.js
@@ -0,0 +1,15 @@
+function loadBanner() {
+  const text = "🍻 Schützenfest 19.06. – 24.06. – Wir freuen uns auf euch!";
+
+  const banner = document.createElement("div");
+  banner.className = "banner";
+
+  banner.innerHTML = `
+    ${text}
+    <span class="banner-close" onclick="this.parentElement.remove()">✕</span>
+  `;
+
+  document.body.prepend(banner);
+}
+
+document.addEventListener("DOMContentLoaded", loadBanner);
