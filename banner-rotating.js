c6840921f9f43e9dec8fa972dc60b09eedf755af
jgz-ak92 <stefan.kauffmann@hotmail.de>
2026-05-17 12:30:44 +0200

    Aktualisieren von banner-rotating.js

--- banner-rotating.js
+++ banner-rotating.js
@@ -1,10 +1,10 @@
 function loadRotatingBanner() {
 
   const messages = [
-    "🍻 Schützenfest 19.06. – 24.06. – Wir freuen uns auf euch! 🍻",
+    "🍻 Schützenfest 19.06. – 24.06. –<br>Wir freuen uns auf euch! 🍻",
     "Wir haben einen neuen Zugkönig...<br>👑 Sascha II. Vidahl 👑",
 "BREAKING NEWS!!! Auch die Damen haben auf den Vogel geschossen! Herzlichen Glückwunsch, 👑 Julia Pulver 👑",
-    " Ein neuer Corpskönig wurde ebenfalls ermittelt, er erringt die Würde bereits zum 2. Mal... Alles Gute, 👑 Sven I. Rommerskirchen 👑 "
+    " Ein neuer Corpskönig wurde ebenfalls ermittelt, er erringt die Würde bereits zum 2. Mal...<br>Alles Gute, 👑 Sven I. Rommerskirchen 👑 "
   ];
  
   
