7e1d3fecf4ea81c95be2b726f7b7b1bd786cfce3
jgz-ak92 <stefan.kauffmann@hotmail.de>
2026-05-17 12:24:53 +0200

    Aktualisieren von termine.js

--- termine.js
+++ termine.js
@@ -1,5 +1,5 @@
 const termine = [
-  {
+ /* {
     monat: "Mai 2026",
     items: [
       {
@@ -8,7 +8,7 @@ const termine = [
         ort: "Alter Sportplatz Hoisten"
       }
     ]
-  },
+  }, */
   {
     monat: "Juni 2026",
     items: [
