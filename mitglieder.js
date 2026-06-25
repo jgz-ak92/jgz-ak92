ababf7b28f5c3c6aa6c1444fc6c5f86d700194a0
jgz-ak92 <stefan.kauffmann@hotmail.de>
2026-06-25 10:42:43 +0200

    Update mitglieder.js

--- mitglieder.js
+++ mitglieder.js
@@ -1,55 +1,118 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const container = document.getElementById("mitglieder-container");
-  const lightbox = document.getElementById("lightbox");
-  const lightboxImg = document.getElementById("lightbox-img");
 
-  fetch("bilder/mitglieder/mitglieder.json")
-    .then(response => response.json())
-    .then(mitglieder => {
-
-      container.innerHTML = "";
+const container =
+document.getElementById("mitglieder-container");
 
 
-      mitglieder
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .forEach(mitglied => {
+const lightbox =
+document.getElementById("lightbox");
+
+const lightboxImg =
+document.getElementById("lightbox-img");
+
+const close =
+document.querySelector(".lightbox-close");
 
 
-          const card = document.createElement("div");
-          card.className = "mitglied-card";
+
+fetch("bilder/mitglieder/mitglieder.json")
+
+.then(response => response.json())
+
+.then(mitglieder => {
 
 
-          card.innerHTML = `
-
-            <img 
-              src="${mitglied.bild}" 
-              alt="${mitglied.name}"
-              loading="lazy"
-            >
-
-            <div class="mitglied-info">
-
-              <h3>${mitglied.name}</h3>
-
-              <p>${mitglied.funktion}</p>
-
-              <small>
-                Mitglied seit ${mitglied.eintritt}
-              </small>
-
-            </div>
-
-          `;
+mitglieder.forEach(mitglied => {
 
 
-          container.appendChild(card);
+const card =
+document.createElement("div");
 
-        });
 
-    })
-    .catch(error => {
-      console.error("Fehler beim Laden der Mitglieder:", error);
-    });
+card.className =
+"mitglied-card";
+
+
+
+card.innerHTML = `
+
+<img 
+src="${mitglied.bild}"
+alt="${mitglied.name}"
+loading="lazy">
+
+
+<div class="mitglied-info">
+
+<h3>${mitglied.name}</h3>
+
+<p>${mitglied.funktion}</p>
+
+<small>
+Mitglied seit ${mitglied.eintritt}
+</small>
+
+</div>
+
+`;
+
+
+
+const img =
+card.querySelector("img");
+
+
+img.addEventListener(
+"click",
+() => {
+
+lightbox.classList.add("active");
+
+lightboxImg.src =
+mitglied.bild;
+
+});
+
+
+
+container.appendChild(card);
+
+
+
+});
+
+
+});
+
+
+
+close.addEventListener(
+"click",
+() => {
+
+lightbox.classList.remove("active");
+
+lightboxImg.src="";
+
+});
+
+
+
+lightbox.addEventListener(
+"click",
+(e)=>{
+
+if(e.target === lightbox){
+
+lightbox.classList.remove("active");
+
+lightboxImg.src="";
+
+}
+
+});
+
+
 
 });
